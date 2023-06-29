b706af59ef9daf80d228b36f6547d41ce830258d
abrhmax <admax3@soy.sena.edu.co>
2023-06-28 19:38:50 -0500

    react

---
+++ App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{fontSize:20,}}>
+      <Text>Hola Mun</Text>
+    </View>
+  );
+};
+
+export default App;
